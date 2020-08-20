0240111174a7149eecaf8054a416fa7dcc738b03
nianhao <nianhaox@qq.com>
2020-08-20 15:45:07 +0800

    在有序二维数组中找到target

--- src/com/leetcode/SwordToOffer/num2.java
+++ src/com/leetcode/SwordToOffer/num2.java
@@ -1,4 +1,21 @@
 package com.leetcode.SwordToOffer;
 
 public class num2 {
+    public boolean findNumberIn2DArray(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        int row = 0;
+        int col = matrix[0].length-1;
+        while (row< matrix.length&&col>=0){
+            if(target== matrix[row][col]) return true;
+            else if(target<matrix[row][col]) col--;
+            else row++;
+        }
+
+        return false;
+
+    }
+
+
 }
